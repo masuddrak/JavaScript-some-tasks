f65fca5cc7f6acd2105612f7c3187e68096b6e1a
masud <masudranaacb@gmail.com>
2024-01-26 14:41:02 +0600

    average marks

--- basic/basic.js
+++ basic/basic.js
@@ -1,6 +1,7 @@
-var myMeny=1000;
-var appleKg=300;
-var orengKg=400;
-var totalConst=appleKg*1+orengKg*1;
-var returnByMerkste=myMeny-totalConst;
-console.log(returnByMerkste)
+var mathematics=75.25;
+var Biology=65;
+var chemistry=80;
+var physics=35.45;
+var bangla=99.50;
+var averageMarks=(mathematics+Biology+chemistry+physics+bangla)/5;
+console.log(averageMarks.toFixed(2))
