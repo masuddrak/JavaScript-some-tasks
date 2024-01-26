8d8673ec84f188b846895d448702f48756ae0cf3
masud <masudranaacb@gmail.com>
2024-01-26 14:48:25 +0600

    reminder

--- basic/basic.js
+++ basic/basic.js
@@ -1,7 +1,3 @@
-var mathematics=75.25;
-var Biology=65;
-var chemistry=80;
-var physics=35.45;
-var bangla=99.50;
-var averageMarks=(mathematics+Biology+chemistry+physics+bangla)/5;
-console.log(averageMarks.toFixed(2))
+var myNumber=119;
+var reminderNunber=myNumber%5;
+console.log(reminderNunber)
