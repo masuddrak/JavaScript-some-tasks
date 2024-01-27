3098f127c9c31f18cdb1ba9d1c1f437c9020ddcf
masud <masudranaacb@gmail.com>
2024-01-27 11:04:05 +0600

    burger problem slov

---
+++ basic/js-conditional-taks.js
@@ -0,0 +1,7 @@
+let burger=400;
+if(burger>=500){
+    console.log('burger+free coke')
+}
+else{
+    console.log('burger+30tk')
+}
--- basic/ternary.js
+++ basic/ternary.js
@@ -15,4 +15,14 @@ const isLeader = true;
 //     price = price + 100;
 // }
 price = isLeader === false ? price<=1000?0:price/2 :price+100;
-console.log(price)
+// console.log(price)
+let x = 5>'4'
+// console.log(x)
+let score = 75;
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+}
+console.log(grade);
