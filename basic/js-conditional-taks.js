7aa812ebe6688e3a9d8185031a7fb374ce4e24f2
masud <masudranaacb@gmail.com>
2024-01-27 12:18:36 +0600

    if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.

--- basic/js-conditional-taks.js
+++ basic/js-conditional-taks.js
@@ -1,16 +1,5 @@
-const grade = 55;
-if (grade >= 90 && grade <= 100) {
-    console.log('your grade is : A')
-}
-else if (grade >= 80 && grade < 90) {
-    console.log("your grade is : B")
-}
-else if (grade >= 70 && grade < 80) {
-    console.log("your grade is : C")
-}
-else if (grade >= 60 && grade < 70) {
-    console.log("your grade is : D")
-}
-else {
-    console.log("Your  Grade is : F")
-}
+const num1=20;
+const num2=10;
+let result;
+result = num1>num2? num1*2: num1+num2;
+console.log(result)
