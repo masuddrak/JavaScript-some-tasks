e9c15fde653b8de3b0ef506250838208c9ed0e7e
masud <masudranaacb@gmail.com>
2024-01-28 02:02:52 +0600

    array slice splice using

--- Array/array.js
+++ Array/array.js
@@ -1,6 +1,9 @@
-const numbers=[10,15,30,40,45];
+// const numbers=[10,15,30,40,45,50];
+//  const sliceUsing=numbers.slice(2,5)
+// console.log(sliceUsing)
+
+const numbers=[10,15,30,40,45,50];
 console.log(numbers)
-numbers.unshift(55,88)
-console.log(numbers)
-numbers.shift()
+const spliceUsing=numbers.splice(2,2,100,140);
+console.log(spliceUsing)
 console.log(numbers)
