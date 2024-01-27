a0d91011d2d84dc8d752cb87dbeef934f4f520e1
masud <masudranaacb@gmail.com>
2024-01-27 11:47:17 +0600

    BMI formula apply solve

--- basic/js-conditional-taks.js
+++ basic/js-conditional-taks.js
@@ -1,7 +1,16 @@
-let burger=400;
-if(burger>=500){
-    console.log('burger+free coke')
+const weight=90;
+const height=1.61;
+const BMI=weight/(height*2);
+console.log(BMI)
+if(BMI<18.5){
+    console.log('are you rnderweight')
+}
+else if(BMI>18.5 && BMI <=24.9){
+    console.log("you are a normal")
+}
+else if(BMI>=25 && BMI<=29.9){
+console.log("you are overweight")
 }
 else{
-    console.log('burger+30tk')
+    console.log('otherwise, you are obese')
 }
