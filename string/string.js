f28921c3d5859802cfca6c59eed4bd709309ffb7
masud <masudranaacb@gmail.com>
2024-01-31 00:03:24 +0600

    using trim,toLowerCase

---
+++ string/string.js
@@ -0,0 +1,15 @@
+// condition check in string toLowerCase
+// const fristName="Ali";
+// const lastName="ali";
+// if(fristName.toLowerCase===lastName.toLowerCase){
+//     console.log('My nane is prefect working')
+// }else{
+//     console.log("is not concat name")
+// }
+const liqud=" pani"
+const water="pani "
+if(liqud.trim()===water.trim()){
+    console.log("right")
+}else{
+    console.log("warang")
+}
