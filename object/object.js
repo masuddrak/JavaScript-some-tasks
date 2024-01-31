f58c68185e3dbff9a31385ba644b37554fb3d87a
masud <masudranaacb@gmail.com>
2024-01-31 13:56:57 +0600

    taks 5 complet

--- object/object.js
+++ object/object.js
@@ -1,10 +1,19 @@
-// Count the number of properties.
-let student = {
-    name: 'Ariana Grande',
-    age: 21,
-    city: 'Gaibandha',
-    isStudent: true
-};
+// Loop through an object and print the key-value pairs with their types
 
-const keys=Object.keys(student)
-console.log(keys.length)
+// Output:
+// key: name | type:  string
+// key: age | type:  number
+// key: city | type:  string
+// key: isStudent | type:  boolean
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+    };
+let keys=Object.keys(myObject)
+for(let element of keys){
+    console.log("Key :",element,"|","type : ",typeof myObject[element])
+}
+// console.log(keys)
