11398d53589bf2b5e905fcdd2a87027b1ad02191
masud <masudranaacb@gmail.com>
2024-01-31 12:47:41 +0600

    object in for in loop apply

--- object/object.js
+++ object/object.js
@@ -1,14 +1,9 @@
-const student={
-    name:'ROhim',
-    roll:455356,
-    age:25,
-    "favorite subject":["physics, chemistry"]
+const mobile={
+    brand:"Samsung",
+    color:"Black",
+    model:"M220 Pro",
+    price:"$5555"
 }
-let favSub='favorite subject'
-
-student[favSub]=['alu, potol, sim']
-
-let ages="age"
-student[ages]=55
-
-console.log(student)
+for(const element in mobile){
+    console.log(element,":",mobile[element])
+}
