e2e472ab601af970b9d0b898445403c765d266a7
masud <masudranaacb@gmail.com>
2024-01-31 14:07:19 +0600

    string task 1

---
+++ object/string.js
@@ -0,0 +1,8 @@
+const title="Bnagladesh PAkistan"
+let findArry=''
+for(let aLter of title){
+    if(aLter.toLowerCase()==="a"){
+        findArry=findArry+aLter
+    }
+}
+console.log(findArry.length)
