e32edea35e5001f35dab6d36b593f63d443e9650
masud <masudranaacb@gmail.com>
2024-01-31 12:13:15 +0600

    sobject access type

---
+++ object/object.js
@@ -0,0 +1,14 @@
+const student={
+    name:'ROhim',
+    roll:455356,
+    age:25,
+    "favorite subject":["physics, chemistry"]
+}
+let favSub='favorite subject'
+
+student[favSub]=['alu, potol, sim']
+
+let ages="age"
+student[ages]=55
+
+console.log(student)
