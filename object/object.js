5d2cfe6b418fd3c565dfb2cf43419f8a1e3b513c
masud <masudranaacb@gmail.com>
2024-01-31 13:39:22 +0600

    taks 2 complet

--- object/object.js
+++ object/object.js
@@ -1,7 +1,6 @@
-const colors = {
-    red: "#ff0000",
-    green: "#00ff00",
-    blue: "#0000ff",
-    "golden rod": '#daa520'
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    "passenger capacity":5,
 };
-console.log(colors["golden rod"])
