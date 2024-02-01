032db30e4bb8f5342c7c4681ad3237f75bbdab4a
masud <masudranaacb@gmail.com>
2024-02-01 10:19:30 +0600

    array loop task 3

--- array-looping-task/array-looping-task.js
+++ array-looping-task/array-looping-task.js
@@ -1,10 +1,8 @@
-//Write a JavaScript code to get the even numbers from an array using any looping technique.
+//Use a for...of loop to concatenate all the elements of an array into a single string.
 
- const numbers = [12, 98, 5, 41, 23, 78, 46];
- let evenNumbers=[]
- for(const number of numbers){
-    if(number%2===0){
-        evenNumbers.push(number)
-    }
- }
- console.log(evenNumbers)
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+var stringNumbers=''
+for(var number of numbers){
+    stringNumbers +=number
+}
+console.log(stringNumbers)
