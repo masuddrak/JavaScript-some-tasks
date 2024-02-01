db9345100563d17a8816a725f0c5e9a2a8497548
masud <masudranaacb@gmail.com>
2024-02-01 10:14:50 +0600

    array looping task 2

--- array-looping-task/array-looping-task.js
+++ array-looping-task/array-looping-task.js
@@ -1,8 +1,10 @@
-// Write a JavaScript code to reverse the array colors without using the reverse method.
-const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+//Write a JavaScript code to get the even numbers from an array using any looping technique.
 
-let reversColor=[];
-for(const color of colors){
-    reversColor.unshift(color)
-}
-console.log(reversColor)
+ const numbers = [12, 98, 5, 41, 23, 78, 46];
+ let evenNumbers=[]
+ for(const number of numbers){
+    if(number%2===0){
+        evenNumbers.push(number)
+    }
+ }
+ console.log(evenNumbers)
