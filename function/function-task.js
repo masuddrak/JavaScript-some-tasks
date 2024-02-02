b4c17e024e418362ea8a7f916e82853201631e50
masud <masudranaacb@gmail.com>
2024-02-03 02:46:14 +0600

    task 4

--- function/function-task.js
+++ function/function-task.js
@@ -1,13 +1,14 @@
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
 
-// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
-function make_avg(numbers) {
-    console.log(numbers.length)
-    let sum=0
-    for(const number of numbers){
-         sum +=number
+function count_zero(str) {
+    let numberOf0 = ''
+    for (let SigleStr of str) {
+        if (SigleStr === "0") {
+            console.log(SigleStr)
+            numberOf0 += SigleStr
+        }
     }
-    const average=sum/numbers.length
-    return average;
+    return numberOf0.length
 }
-const resut=make_avg([1,2,3,4])
-console.log(resut)
+const str0 = count_zero("0101001")
+console.log(str0)
