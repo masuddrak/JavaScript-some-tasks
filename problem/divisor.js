f36b2d8ab6e3a8d0224dac5bed18bcb567b2eaee
masud <masudranaacb@gmail.com>
2024-02-02 12:52:49 +0600

    top code problem

---
+++ problem/divisor.js
@@ -0,0 +1,8 @@
+//Read an integer variable and print all of its divisors (including 1 and the number itself).
+let number=6;
+for(let i=1; i<=6; i++){
+    if(number%i===0){
+        console.log(i)
+    }
+    
+}
