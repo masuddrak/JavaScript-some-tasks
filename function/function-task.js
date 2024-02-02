5f4e662f77a4581908bf2233f6c8a7cd307af33e
masud <masudranaacb@gmail.com>
2024-02-03 02:07:31 +0600

    task 1

---
+++ function/function-task.js
@@ -0,0 +1,8 @@
+
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(a,b,c,d){
+return a*b*c*d
+}
+const multiplyNumber=multiply(2,3,4,5)
+console.log(multiplyNumber)
