5d6dca31b1968f55568e228c40fc45dd607924fb
masud <masudranaacb@gmail.com>
2024-02-03 23:42:18 +0600

    leap year check

--- problem/divisor.js
+++ problem/divisor.js
@@ -1,8 +1,15 @@
-//Read an integer variable and print all of its divisors (including 1 and the number itself).
-let number=6;
-for(let i=1; i<=6; i++){
-    if(number%i===0){
-        console.log(i)
+// Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400. For example, the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are.
+
+function ieapYear(year){
+    if(year%100 !== 0 && year%4===0){
+        return true
     }
-    
+    else if(year%400===0){
+        return true
+    }
+    else{
+        return "is not leap year"
+    }
 }
+const year=ieapYear(1900)
+console.log(year)
