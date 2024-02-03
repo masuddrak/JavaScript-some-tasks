5bf7b395068b62fd6f3380f27ce36d00973e46c6
masud <masudranaacb@gmail.com>
2024-02-04 00:11:38 +0600

    remove duplicate number in an array

--- problem/divisor.js
+++ problem/divisor.js
@@ -1,15 +1,14 @@
-// Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400. For example, the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are.
+// Duplicate remove the array
 
-function ieapYear(year){
-    if(year%100 !== 0 && year%4===0){
-        return true
+function removDuplicate(numbers){
+    const removeDuplicateNumber=[]
+    for(let number of numbers){
+        if(removeDuplicateNumber.includes(number) !== true){
+            removeDuplicateNumber.push(number)
+        }
     }
-    else if(year%400===0){
-        return true
-    }
-    else{
-        return "is not leap year"
-    }
+    return removeDuplicateNumber
 }
-const year=ieapYear(1900)
-console.log(year)
+const numbers=[12,58,48,3,4,7,8,48,58,7,7,7,7]
+const result=removDuplicate(numbers)
+console.log(result)
