7c2e324590758d08d07e878dd592f35638b61180
masud <masudranaacb@gmail.com>
2024-02-05 13:43:00 +0600

    discount layer by quintity

--- function-problem/function-problem.js
+++ function-problem/function-problem.js
@@ -1,16 +1,26 @@
 function discundPrince(quntity){
-    if(quntity<=100){
-        const total=quntity*100
-        return total
-    }
+   const quiltity100=100
+   const quiltity200=90
+   const avobe200=70
+   if(quntity<=100){
+    const totalPrice=quntity*quiltity100
+    return totalPrice
+   }
+   else if(quntity<=200 ){
+        const frist100TOtal=quiltity100*100
+        const remainingQuintity=quntity-100
+        const secontPrice=remainingQuintity*quiltity200
+        const totl=frist100TOtal+secontPrice
+        return totl
+   }
+   else{
+    const frist100TOtal=quiltity100*100
+    const second100TOtal=quiltity200*100
+    const remainingQuintity=quntity-200
+    const third200Avobe=remainingQuintity*avobe200
+    const totl=frist100TOtal+second100TOtal+third200Avobe
+    return totl
 
-   else if(quntity>100 && quntity<=200){
-        const total=quntity*90
-        return total
-    }
-    else{
-        const total=quntity*70
-        return total
-    }
+   }
 }
-console.log(discundPrince(200))
+console.log(discundPrince(202))
