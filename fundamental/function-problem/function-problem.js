8eeac6173d5599d99ced73454a05927d62fb0b72
masud <masudranaacb@gmail.com>
2024-02-06 14:11:21 +0600

    tast 4

--- fundamental/function-problem/function-problem.js
+++ fundamental/function-problem/function-problem.js
@@ -1,13 +1,22 @@
-// Task -2:
-// Find the friend with the smallest name.
-// const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
-
-const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
-let smallestName=heights2[0]
-// console.log(smallestName.length)
-for(const name of heights2){
-    if(smallestName.length>name.length){
-        smallestName=name
+// Task-4:
+// You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
+function findAveragePhonePrice(phones){
+    let priceArray=0
+    for(const phone of phones){
+        priceArray+=phone.price
     }
+const averag=priceArray/phones.length
+console.log(priceArray)
+return averag
+
 }
-console.log(smallestName)
+const phones = [
+    { model: "PhoneA", brand: "Iphone", price: 95000 },
+    { model: "PhoneB", brand: "Samsung", price: 40000 },
+    { model: "PhoneC", brand: "Oppo", price: 26000 },
+    { model: "PhoneD", brand: "Nokia", price: 35000 },
+    { model: "PhoneE", brand: "Iphone", price: 105000 },
+    { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+const result=findAveragePhonePrice(phones)
+console.log(result)
