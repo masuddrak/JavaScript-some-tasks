dd22a43a4de273868fc819c426d71cd1d73331d1
masud <masudranaacb@gmail.com>
2024-03-07 07:36:49 +0600

    added object keys and values

---
+++ fundamental/recap/recap.js
@@ -0,0 +1,8 @@
+const sudent={
+    name:"masud",
+    age:20,
+    hovis:["cricket","footbal"]
+}
+const sudentKeys=Object.keys(sudent)
+const sudentVlue=Object.values(sudent)
+console.log(sudentKeys,sudentVlue)
